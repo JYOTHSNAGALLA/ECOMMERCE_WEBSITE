94a27954c7ceafcda0d022979e07c381576a28ff
JYOTHSNAGALLA <gallajyothsna13@gmail.com>
2025-07-13 22:23:05 +0530

    Reorganized server structure and added structure.txt

---
+++ server/server/model/UserModel.js
@@ -0,0 +1,23 @@
+// model/UserModel.js
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ecomDbConnection = mongoose.connection.useDb('ecomDb');
+
+const userSchema = new Schema({
+  name: { type: String, required: true, trim: true },
+  phone: { type: String, required: true, unique: true, trim: true },
+  password: { type: String, required: true, trim: true },
+  address: { type: String, trim: true },
+  dp: {
+    type: String,
+    default: 'https://placehold.co/100x100/A0B0C0/FFFFFF?text=DP'
+  },
+  cart: [{ type: Schema.Types.ObjectId, ref: 'Product' }],
+  order: [{ type: Schema.Types.ObjectId, ref: 'Order' }],
+  history: [{ type: Schema.Types.ObjectId, ref: 'PurchaseBook' }]
+}, {
+  timestamps: true
+});
+
+const UserModel = ecomDbConnection.model('User', userSchema);
+module.exports = UserModel;
