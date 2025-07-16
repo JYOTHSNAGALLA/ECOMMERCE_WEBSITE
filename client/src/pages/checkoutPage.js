a4df6b03d38ae86bbc8665aec67936cca2d5096e
JYOTHSNAGALLA <gallajyothsna13@gmail.com>
2025-07-16 22:29:30 +0530

    Fix: update the checkout page

--- client/src/pages/checkoutPage.js
+++ client/src/pages/checkoutPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/checkout.css"; // <-- CSS file
+import "../App.css"; // <-- CSS file
 
 function checkoutPage() {
   const navigate = useNavigate();
