8374fe5ca60ed971688deadc35d5dff0ff3ec2c3
JYOTHSNAGALLA <gallajyothsna13@gmail.com>
2025-07-19 20:15:14 +0530

    Fix: update app.js and index js

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,21 @@
-// index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter } from "react-router-dom";
 import { CartProvider } from './context/CartContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <CartProvider>
         <App />
       </CartProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
+// Optional performance reporting
 reportWebVitals();
